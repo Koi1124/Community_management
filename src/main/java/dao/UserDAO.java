d27a26148f1b5df2282d6ec7290fe92a698651a6
LyeDream <50206113@qq.com>
2018-07-10 10:07:01 +0800

    更新 UserDAO.java

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -51,6 +51,9 @@ public interface UserDAO {
      */
     User userInformation(String keyword);
 
+    void initUserList(List<User> ulist);
+    int deleteUser(String deleteNum);
+    void searchUser(List<User>searchList,String uName);
     /**
      *@Discription: 根据用户得到加入社团信息
      *@Param: [stuNum]
