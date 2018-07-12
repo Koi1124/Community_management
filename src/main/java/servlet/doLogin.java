03a6f3fa604194f6c5e4300d8943335e7ce6d6fd
LyeDream <50206113@qq.com>
2018-07-12 16:29:10 +0800

    更新 doLogin.java

--- src/main/java/servlet/doLogin.java
+++ src/main/java/servlet/doLogin.java
@@ -36,6 +36,7 @@ public class doLogin extends HttpServlet {
     UserService userService = new UserService();
     ActivityService activityService = new ActivityService();
     CommunityService communityService=new CommunityService();
+    UploadService uploadService = new UploadService();
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //UserDAOImp muImp = new UserDAOImp();
         response.setContentType("text/html;charset=utf-8");
@@ -230,71 +231,12 @@ public class doLogin extends HttpServlet {
         comm.setcSrc(relPath);
         boolean createResult=communityService.createApply(comm);
         if(createResult) {
-            SaveCutImage(base64img,path,workPath);
+            uploadService.SaveCutImage(base64img,path,workPath);
         }
 
         return createResult;
     }
 
-    public BufferedImage toBufferedImage(Image image) {
-        if (image instanceof BufferedImage) {
-            return (BufferedImage) image;
-        }
-        image = new ImageIcon(image).getImage();
-        BufferedImage bimage = null;
-        GraphicsEnvironment ge = GraphicsEnvironment
-                .getLocalGraphicsEnvironment();
-        try {
-            int transparency = Transparency.OPAQUE;
-            GraphicsDevice gs = ge.getDefaultScreenDevice();
-            GraphicsConfiguration gc = gs.getDefaultConfiguration();
-            bimage = gc.createCompatibleImage(image.getWidth(null),
-                    image.getHeight(null), transparency);
-        } catch (HeadlessException e) {
-            // The system does not have a screen
-        }
-        if (bimage == null) {
-            // Create a buffered image using the default color model
-            int type = BufferedImage.TYPE_INT_RGB;
-            bimage = new BufferedImage(image.getWidth(null),
-                    image.getHeight(null), type);
-        }
-        // Copy image to buffered image
-        Graphics g = bimage.createGraphics();
-        // Paint the image onto the buffered image
-        g.drawImage(image, 0, 0, null);
-        g.dispose();
-        return bimage;
-    }
-
-
-
-    //保存裁剪图片
-    public  void SaveCutImage (String base64, String address,String workPath)
-    {
-        BASE64Decoder decoder = new BASE64Decoder();
-        byte[] b = new byte[0];//转码得到图片byte数组
-        try {
-            b = decoder.decodeBuffer(base64);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-
-        try {
-            Image imageTookit = Toolkit.getDefaultToolkit().createImage(b);
-            BufferedImage bi = toBufferedImage(imageTookit);
-            File w2 = new File(address);
-            File ww= new File(workPath);
-            ImageIO.write(bi, "png", w2);
-            ImageIO.write(bi, "png", ww);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
+   
 
 }
